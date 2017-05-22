ebe2af962241c1e09fa8e9e3f92c2adf86ed097e
SuryaElavazhagan <surya.elash97@outlook.com>
2017-05-23 00:05:52 +0530

    Update MainActivity.java

--- CoffeeTask2/app/src/main/java/com/example/surya/coffeework/MainActivity.java
+++ CoffeeTask2/app/src/main/java/com/example/surya/coffeework/MainActivity.java
@@ -1,6 +1,12 @@
-package com.example.surya.coffeetask;
+package com.example.surya.coffeework;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,77 +16,117 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button userBtn, driverBtn;
-    public EditText editText;
-    public Button button;
+    private Button BtnSubmit;
+    private RadioGroup radioGroup;
+    private EditText EtName;
+    private int i =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.edit_text);
-        userBtn = (Button) findViewById(R.id.user_btn);
-        driverBtn = (Button) findViewById(R.id.driver_btn);
+        BtnSubmit = (Button) findViewById(R.id.btn_submit);
+        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        EtName = (EditText) findViewById(R.id.et_name);
 
-        userBtn.setOnClickListener(new View.OnClickListener() {
+
+
+        BtnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(editText.getText().toString().length() > 0 ){
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                boolean gpsEnabled = false;
+                boolean networkEnabled = false;
 
-                    String userName = editText.getText().toString();
-                DatabaseHelper.root.child("Users").child("UserName").setValue(userName);
-                Intent intent = new Intent(MainActivity.this, MapsActivityUser.class);
-                    intent.putExtra("user name", userName);
-                startActivity(intent);
+                try{
+                    gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                }catch (Exception ex){}
+                try{
+                    networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                }catch (Exception ex){}
 
-            }
-            else
-            {
-                Toast.makeText(MainActivity.this , "Enter your name!" , Toast.LENGTH_LONG).show();
-            }
-            }
-        });
-
-        driverBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(editText.getText().toString().length() > 0 ){
-
-                    String driverName = editText.getText().toString();
-                    DatabaseHelper.root.child("Drivers").child("DriverName").setValue(driverName);
-                    final Intent intent = new Intent(MainActivity.this, MapsActivityDriver.class);
-                    DatabaseReference trigger = DatabaseHelper.root.child("Users").child("LatLng");
-                    trigger.addListenerForSingleValueEvent(new ValueEventListener() {
+                if(!gpsEnabled && !networkEnabled){
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                    dialog.setMessage("Your Location is off!");
+                    dialog.setPositiveButton("Open Settings", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            String data = dataSnapshot.getValue(String.class);
-                      Toast.makeText(MainActivity.this, data ,Toast.LENGTH_LONG  ).show();
-                            intent.putExtra("data" , data);
-                            startActivity(intent);
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            MainActivity.this.startActivity(intent);
                         }
                     });
 
+                    dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+
+                }
+
+            if(gpsEnabled && networkEnabled){
+
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                String name = EtName.getText().toString();
+                if((name.length() > 0)  && (selectedId > 0))
+                {
+                    RadioButton radioButton = (RadioButton) findViewById(selectedId);
+                    String choice = radioButton.getText().toString();
+
+                    if(choice.equals("User")) {
+
+                        DatabaseReference userNode  = FirebaseDatabase.getInstance().getReference().getRoot().child("User");
+                        userNode.setValue(null);
+                        DatabaseReference root = FirebaseDatabase.getInstance().getReference();
+                        DatabaseReference mUsers = root.child("User");
+                        mUsers.child("Name").setValue(name);
+                        Intent intent = new Intent(MainActivity.this , MapsActivityUser.class);
+                        startActivity(intent);
+                    }
+
+                    if(choice.equals("Driver")){
+                        DatabaseReference driverNode  = FirebaseDatabase.getInstance().getReference().getRoot().child("Driver");
+                        driverNode.setValue(null);
+
+                        DatabaseReference root = FirebaseDatabase.getInstance().getReference();
+                        DatabaseReference mDrivers = root.child("Driver");
+                        mDrivers.child("Name").setValue(name);
+                        DatabaseReference mUserLocationUpdate = root.child("User").child("Location");
+                        mUserLocationUpdate.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                double latitude = (double) dataSnapshot.child("Latitude").getValue();
+                                double longitude = (double) dataSnapshot.child("Longitude").getValue();
+                                Intent intent = new Intent(MainActivity.this,MapsActivityDriver.class);
+                                intent.putExtra("Latitude" , latitude);
+                                intent.putExtra("Longitude" , longitude);
+                                startActivity(intent);
+                                }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
                 }
                 else
                 {
-                    Toast.makeText(MainActivity.this , "Enter your name!" , Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this,"Provide the Credentials" , Toast.LENGTH_LONG).show();
                 }
+            }
             }
         });
 
